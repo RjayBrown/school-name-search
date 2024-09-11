f28f46ad1cfe74c0b5832764ebe4408c6e2b24b9
RjayBrown <rjbrown.dev@outlook.com>
2024-09-10 23:08:20 -0400

    added school info section

--- js/main.js
+++ js/main.js
@@ -1,22 +1,29 @@
 const btn = document.querySelector('button')
 const input = document.querySelector('input')
 const list = document.querySelector('.results')
+const schoolInfo = document.querySelector('.school-info')
 
 
 btn.addEventListener('click', getSchools)
 
 async function getSchools() {
     list.innerHTML = ''
+
     const value = input.value
 
-    const res = await fetch(`https://api.data.gov/ed/collegescorecard/v1/schools?api_key=fdBYetUZF2CVd84OoqrvdYSCZar2Zdxxb8HkZyAy&school.name=${value}&fields=id,school.name,latest.student.size`)
+    const res = await fetch(`https://api.data.gov/ed/collegescorecard/v1/schools?api_key=fdBYetUZF2CVd84OoqrvdYSCZar2Zdxxb8HkZyAy&school.name=${value}`)
     const data = await res.json()
     const schoolList = data.results
     schoolList.forEach((school) => {
-        // console.log(school)
+        // console.log(school.latest.school.name)
+        let schoolName = school.latest.school.name
         const liEl = document.createElement('li')
         list.appendChild(liEl)
-        liEl.innerHTML = `<a href="#">${Object.values(school)[1]}</a>`
+        liEl.innerHTML = `${schoolName}`
+        // liEl.addEventListener('click', getSchoolInfo)
     })
     console.log(schoolList)
 }
+
+// function getSchoolInfo() {
+// }
